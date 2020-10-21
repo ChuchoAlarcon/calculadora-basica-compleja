3de13b5e5e938daa3df139a5f598129d9cc57635
JulioCCondeB <julioconde12496@gmail.com>
2020-10-20 21:36:37 -0400

    Calculadora Conde

--- Complejas/Calcu.java
+++ Complejas/Calcu.java
@@ -1,11 +1,24 @@
 package Complejas;
 
 public class Calcu {
-    public void factorial() {
+
+    public Calcu(){
 
     }
+    
+    public int factorial(int a) {
+        int fact = 1;
+        for (int i = 1; i <= a; i++){
+            fact *= i; 
+        }
+        return fact;
+    }
 
-    public void potencia() {
-
+    public int potencia(int a, int b) {
+        int pot = 1;
+        for(int i = 0; i < b; i++ ){
+            pot *= a;
+        }
+        return pot;
     }
 }
